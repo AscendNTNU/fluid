a670b057b4fbc28d60490b96ad4c4ed29aa0644b
Simen Gangstad <simen.gangstad@me.com>
2018-11-17 15:31:53 +0100

    Remove unnecessary imports from main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,8 @@
 // Created by simengangstad on 27.09.18.
 //
 
-#include "../include/core/operation/operation.h"
+#include "../include/actionlib/operation_server.h"
 #include <ros/ros.h>
-#include <list>
-#include <fluid_fsm/OperationGoal.h>
-#include <fluid_fsm/OperationAction.h>
-#include "actionlib/operation_server.h"
-
-#include "../include/states/init_state.h"
-#include "../include/states/take_off_state.h"
-#include "../include/states/hold_state.h"
-#include "../include/core/state.h"
-#include "../include/core/transition.h"
-
-#include <actionlib/client/simple_action_client.h>
 
 int main(int argc, char** argv) {
 
