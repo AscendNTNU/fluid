6f8b61fe19a420ce14d958297d8cdfb4d706bb40
Simen Gangstad <simen.gangstad@me.com>
2018-11-17 15:28:24 +0100

    Remove unnecessary imports from main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,16 @@
 // Created by simengangstad on 27.09.18.
 //
 
-#include "../include/core/operation/operation.h"
 #include <ros/ros.h>
-#include <list>
-#include <fluid_fsm/OperationGoal.h>
-#include <fluid_fsm/OperationAction.h>
-#include "actionlib/operation_server.h"
-
-#include "../include/states/init_state.h"
-#include "../include/states/take_off_state.h"
-#include "../include/states/hold_state.h"
-#include "../include/core/state.h"
-#include "../include/core/transition.h"
-
-#include <actionlib/client/simple_action_client.h>
+#include <memory>
+#include <actionlib/server/simple_action_server.h>
+#include "../include/actionlib/operation_server.h"
 
 int main(int argc, char** argv) {
 
     ros::init(argc, argv, "fluid_fsm");
+
+    std::cout << "Hello World" << std::endl;
 
     fluid::OperationServer operation_server;
     operation_server.execute();
