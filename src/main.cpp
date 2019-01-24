410d2bd3490822cb7f46791ce8f284518cd215d6
Simen Gangstad <simen.gangstad@me.com>
2019-01-24 09:18:10 +0100

    Merge change

--- src/main.cpp
+++ src/main.cpp
@@ -4,27 +4,13 @@
 
 #include "../include/actionlib/operation_server.h"
 #include <ros/ros.h>
-<<<<<<< HEAD
-#include <ostream>
-=======
-#include <iostream>
-#include <memory>
->>>>>>> develop
 
 int main(int argc, char** argv) {
 
     ros::init(argc, argv, "fluid_fsm");
 
-<<<<<<< HEAD
-    std::cout << "Hello World" << std::endl;
-
-    fluid::OperationServer operation_server;
-    operation_server.execute();
-
-=======
     fluid::OperationServer operation_server(atoi(argv[1]));
     operation_server.start();
     
->>>>>>> develop
     return 0;
 }
