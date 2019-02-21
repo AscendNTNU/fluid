e2292ec0db81d9d0d3f5f167086050276d30bb19
Simen Gangstad <simen.gangstad@me.com>
2019-02-21 11:34:54 +0100

    Implement a subscriber in ai voice rec client

--- src/voice_recognition_client.cpp
+++ src/voice_recognition_client.cpp
@@ -2,4 +2,37 @@
 // Created by simengangstad on 21.02.19.
 //
 
-#include "voice_recognition_client.h"
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+
+const char* lastCommand;
+
+void commandCallback(const std_msgs::String::ConstPtr& string) {
+    const char* newCommand = string->data.c_str();
+
+    if (lastCommand != newCommand) {
+
+        std::cout << "New command: " << newCommand << std::endl;
+
+        if (newCommand == "alpha") {
+
+        }
+        else if (newCommand == "bravo") {
+
+        }
+
+        lastCommand = newCommand;
+    }
+}
+
+int main(int argc, char** argv) {
+
+    ros::init(argc, argv, "ai_voice_client");
+    ros::NodeHandle node_handle;
+
+	node_handle.subscribe<std_msgs::String>("ai/voice/command", 10, commandCallback);
+
+	ros::spin();
+
+    return 0;
+}
