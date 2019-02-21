aa51189db15941df4b86bcb5295e8d5d52286094
Simen Gangstad <simen.gangstad@me.com>
2019-02-21 17:28:46 +0100

    Add auto arm flag

--- src/main.cpp
+++ src/main.cpp
@@ -13,11 +13,16 @@ int main(int argc, char** argv) {
 
     ros::init(argc, argv, "fluid_fsm");
 
+    ROS_INFO("\n Starting Fluid FSM...");
+
     fluid::Core::refresh_rate = atoi(argv[1]);
     fluid::Core::message_queue_size = atoi(argv[2]);
+    fluid::Core::auto_arm = argv[3] == "true";
 
     fluid::OperationServer operation_server;
     operation_server.start();
-    
+
+    ROS_INFO("Operation server up and running.\n");
+
     return 0;
 }
