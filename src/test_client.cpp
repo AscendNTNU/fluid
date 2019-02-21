37279b4a928e56a131e5fe466bc56698ef8c01e1
Simen Gangstad <simen.gangstad@me.com>
2019-02-21 09:54:54 +0100

    Remove logging

--- src/test_client.cpp
+++ src/test_client.cpp
@@ -55,10 +55,6 @@ int main(int argc, char** argv) {
         wait_rate.sleep();
     }
 
-
-
-    ROS_INFO("Completed initialization and take off");
-
     // Just for demonstration, this will make the drone move in straight lines to form a square. When the current move
     // is finished, the next will execute as one can see in the callback.
     fluid::OperationClient move_operation_client(60);
