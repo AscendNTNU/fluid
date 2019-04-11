7e21a479545f9ff0f2f10457fd8ba42bf6977fc1
Simen Gangstad <simen.gangstad@me.com>
2019-04-11 08:54:08 +0200

    Make take off execute in the current position

--- src/operations/take_off_operation.cpp
+++ src/operations/take_off_operation.cpp
@@ -9,7 +9,13 @@ fluid::TakeOffOperation::TakeOffOperation(mavros_msgs::PositionTarget position_t
 								          Operation(fluid::OperationIdentifier::TakeOff,
 								                    fluid::StateIdentifier::TakeOff,
 								                    fluid::StateIdentifier::Hold,
-								                    position_target) {}
+								                    position_target) {
+
+	// We want that the take off is executed from the current position at ground, so we
+	// grab the current x and y values.
+	position_target.position.x = fluid::Core::getGraphPtr()->current_state_p->getCurrentPose().pose.position.x;
+	position_target.position.y = fluid::Core::getGraphPtr()->current_state_p->getCurrentPose().pose.position.y;
+}
 
 bool fluid::TakeOffOperation::validateOperationFromCurrentState(std::shared_ptr<fluid::State> current_state_p) {
     return current_state_p->identifier == "idle";
