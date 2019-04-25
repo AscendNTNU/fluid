2692e4d65d5b47cc881ab6fe233ba3cde88a8d16
Simen Gangstad <simen.gangstad@me.com>
2019-04-25 12:53:25 +0200

    Update to fetch parameters
    Will fetch parameters from the parameter server initially, not every tick

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,18 @@ int main(int argc, char** argv) {
     fluid::Core::auto_arm = std::string(argv[3]) == "true";
     fluid::Core::auto_set_offboard = std::string(argv[4]) == "true";
 
+	ros::NodeHandle node_handle;
+    
+    node_handle.getParam("minX", fluid::Core::minX);
+    node_handle.getParam("minY", fluid::Core::minY);
+    node_handle.getParam("minZ", fluid::Core::minZ);
+
+    node_handle.getParam("maxX", fluid::Core::maxX);
+    node_handle.getParam("maxY", fluid::Core::maxY);
+    node_handle.getParam("maxZ", fluid::Core::maxZ);
+
+    node_handle.getParam("positionFollowHeight", fluid::Core::positionFollowHeight);
+
     fluid::OperationServer operation_server;
     operation_server.start();
 
