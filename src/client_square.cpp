f5d4327e7381d1c32650d10d5b5ca654c6ba97ec
Simen Gangstad <simen.gangstad@me.com>
2019-05-03 13:38:00 +0200

    Revert client square to be a square test

--- src/client_square.cpp
+++ src/client_square.cpp
@@ -1,93 +1,96 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Pose.h>
 
-#include <chrono>
-#include <ostream>
-#include <string>
+#include <tf2/LinearMath/Quaternion.h>
 
-#include <cstdlib> 
-#include <ctime>     
-#include <memory>
-
-#include "../include/core/operation/operation.h"
 #include "../include/actionlib/operation_client.h"
 #include "../include/operations/operation_identifier.h"
-#include "../include/core/state.h"
-
-std::string name_space;
-float xLength, yLength;
-std::shared_ptr<fluid::OperationClient> operation_client_ptr;
-
-bool initialPoseSet = false;
-geometry_msgs::Pose initialPose;
-geometry_msgs::Pose lastPose;
-geometry_msgs::Pose pose;
-
-constexpr float height = 2.5;
-
-void runOperation(bool completed) {
-
-    // Run two operations:
-    // 
-    // 1. Rotate towards the next position
-    // 2. Move to the next position
-
-    float x = initialPose.position.x + (-xLength + static_cast<float>(rand()) / (static_cast<float>(RAND_MAX/(xLength * 2))));
-    float y = initialPose.position.y + (-yLength + static_cast<float>(rand()) / (static_cast<float>(RAND_MAX/(yLength * 2))));
-    pose.position.x = x;
-    pose.position.y = y;
-    pose.position.z = height;
-
-    operation_client_ptr->requestOperation(fluid::OperationIdentifier::Move, pose, runOperation);
-}
-
-void poseCallback(const geometry_msgs::PoseStampedConstPtr pose) {
-
-    if (!initialPoseSet && fabs(pose->pose.position.x) > 0.5) {
-
-        initialPoseSet = true;
-        initialPose = pose->pose;
-        lastPose = initialPose;
-    }
-}
 
 int main(int argc, char** argv) {
 
-    name_space = std::string(argv[1]);
-    xLength = static_cast<float>(atof(argv[2]));
-    yLength = static_cast<float>(atof(argv[3]));
+    ros::init(argc, argv, "client_square");
+    ros::NodeHandle nh;
 
-    ros::init(argc, argv, name_space + "_random_movement_client");
-    ros::NodeHandle node_handle;
-    operation_client_ptr = std::make_shared<fluid::OperationClient>(name_space, 60);
-    ros::Rate rate(20);
-
-    // Initialization and take off
+    geometry_msgs::Pose pose;
     bool initialized = false;
+    float height = 1.0;
 
-    pose.position.x = 0.0;
-    pose.position.y = 0.0;
-
-    operation_client_ptr->requestOperation(fluid::OperationIdentifier::Init, pose, [&](bool completed) {
+    fluid::OperationClient operation_client("drone_1", 60);
+    
+    operation_client.requestOperation(fluid::OperationIdentifier::Init, pose, [&](bool completed) {
         if (completed) {
 
-            pose.position.z = height;
+            geometry_msgs::Pose take_off_pose;
+            take_off_pose.position.x = 1;
+            take_off_pose.position.y = 1;
+            take_off_pose.position.z = height;
 
-            operation_client_ptr->requestOperation(fluid::OperationIdentifier::TakeOff, pose, [&](bool completed) {
+            operation_client.requestOperation(fluid::OperationIdentifier::TakeOff, take_off_pose, [&](bool completed) {
                 initialized = completed;
             });
         }
     });
 
+
+    ros::Rate wait_rate(20);
+
     while (ros::ok() && !initialized) {
         ros::spinOnce();
-        rate.sleep();
+        wait_rate.sleep();
     }
 
-    // Initialize the random movement
-    srand(static_cast<unsigned int>(time(nullptr)));
-    runOperation(true);
-    ros::spin();
+    // Just for demonstration, this will make the drone move in straight lines to form a square. When the current move
+    // is finished, the next will execute as one can see in the callback.
+    // 
+    float distance = 1;
+
+    pose.position.x = 1 + distance;
+    pose.position.y = 1;
+    pose.position.z = height;
+    
+    operation_client.requestOperation(fluid::OperationIdentifier::MoveOriented, pose, [&](bool completed) {
+        if (completed) {
+                pose.position.x = 1 + distance;
+                pose.position.y = 1 + distance;
+                pose.position.z = height;
+
+                operation_client.requestOperation(fluid::OperationIdentifier::MoveOriented, pose, [&](bool completed) {
+                    if (completed) {
+
+                        pose.position.x = 1;
+                        pose.position.y = 1 + distance;
+                        pose.position.z = height;
+
+                        operation_client.requestOperation(fluid::OperationIdentifier::MoveOriented, pose, [&](bool completed) {
+                            if (completed) {
+                                pose.position.x = 1;
+                                pose.position.y = 1;
+                                pose.position.z = height;
+
+                                operation_client.requestOperation(fluid::OperationIdentifier::MoveOriented, pose, [&](bool completed) {
+                                    if (completed) {
+                                        geometry_msgs::Pose land_pose;
+                                        land_pose.position.x = 1;
+                                        land_pose.position.y = 1;
+                                        land_pose.position.z = 0;
+
+                                        operation_client.requestOperation(fluid::OperationIdentifier::Land, land_pose, [&](bool completed) {});
+
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
+        }
+    });
+
+    ros::Rate rate(1);
+
+    while (ros::ok()) {
+            ros::spinOnce();
+            rate.sleep();
+    }
 
     return 0;
 }
