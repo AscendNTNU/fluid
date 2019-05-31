a73b9f7efb793c3168f803236a92476f40ef4afd
Simen Gangstad <simen.gangstad@me.com>
2019-05-31 14:16:06 +0200

    Improve client square

--- src/client_square.cpp
+++ src/client_square.cpp
@@ -14,21 +14,23 @@ int main(int argc, char** argv) {
     geometry_msgs::Pose pose;
     bool initialized = false;
     float height = 1.0;
+    float startX = 0.0;
+    float startY = 0.0;
 
-    fluid::OperationClient operation_client("drone_1", 60);
+    fluid::OperationClient operation_client("drone_1", 10);
     
     operation_client.requestOperation(fluid::OperationIdentifier::Init, pose, [&](bool completed) {
-        if (completed) {
+        //if (completed) {
 
             geometry_msgs::Pose take_off_pose;
-            take_off_pose.position.x = 1;
-            take_off_pose.position.y = 1;
+            take_off_pose.position.x = startX;
+            take_off_pose.position.y = startY;
             take_off_pose.position.z = height;
 
             operation_client.requestOperation(fluid::OperationIdentifier::TakeOff, take_off_pose, [&](bool completed) {
                 initialized = completed;
             });
-        }
+//        }
     });
 
 
@@ -42,36 +44,36 @@ int main(int argc, char** argv) {
     // Just for demonstration, this will make the drone move in straight lines to form a square. When the current move
     // is finished, the next will execute as one can see in the callback.
     // 
-    float distance = 1;
+    float distance = 5;
 
-    pose.position.x = 1 + distance;
-    pose.position.y = 1;
+    pose.position.x = startX + distance;
+    pose.position.y = startY;
     pose.position.z = height;
     
     operation_client.requestOperation(fluid::OperationIdentifier::MoveOriented, pose, [&](bool completed) {
         if (completed) {
-                pose.position.x = 1 + distance;
-                pose.position.y = 1 + distance;
+                pose.position.x = startX + distance;
+                pose.position.y = startY + distance;
                 pose.position.z = height;
 
                 operation_client.requestOperation(fluid::OperationIdentifier::MoveOriented, pose, [&](bool completed) {
                     if (completed) {
 
-                        pose.position.x = 1;
-                        pose.position.y = 1 + distance;
+                        pose.position.x = startX;
+                        pose.position.y = startY + distance;
                         pose.position.z = height;
 
                         operation_client.requestOperation(fluid::OperationIdentifier::MoveOriented, pose, [&](bool completed) {
                             if (completed) {
-                                pose.position.x = 1;
-                                pose.position.y = 1;
+                                pose.position.x = startX;
+                                pose.position.y = startY;
                                 pose.position.z = height;
 
                                 operation_client.requestOperation(fluid::OperationIdentifier::MoveOriented, pose, [&](bool completed) {
                                     if (completed) {
                                         geometry_msgs::Pose land_pose;
-                                        land_pose.position.x = 1;
-                                        land_pose.position.y = 1;
+                                        land_pose.position.x = startX;
+                                        land_pose.position.y = startY;
                                         land_pose.position.z = 0;
 
                                         operation_client.requestOperation(fluid::OperationIdentifier::Land, land_pose, [&](bool completed) {});
