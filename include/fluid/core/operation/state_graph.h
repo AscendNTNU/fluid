217d2bcdbea83fbb4ebd8aea05073f01abd81ca2
Simen Gangstad <simen.gangstad@me.com>
2019-06-01 23:19:47 +0200

    Merge graph into state graph

--- include/fluid/core/operation/state_graph.h
+++ include/fluid/core/operation/state_graph.h
@@ -9,24 +9,59 @@
 #include <string>
 
 #include "state.h"
-#include "graph.h"
 
 namespace fluid {
+
+    /** \class Edge
+     *  \brief Represents an edge/connection between two nodes a the graph.
+     */
+    template <class T> class Edge {
+    public:
+        
+        const T source;                 ///< The source of the edge
+        const T destination;            ///< The desination of the edge
+        
+        /** Initializes the edge with its respective source and destination.
+         * 
+         */
+        Edge(T source, T destination): source(source), destination(destination) {}
+    };
+
+
+    typedef std::map<std::string, std::vector<std::shared_ptr<State>>> AdjacencyList;
 
     /** \class StateGraph
      * 
      *  \brief Represents a graph with states.
      */
-    class StateGraph: public fluid::Graph {
+    class StateGraph {
+
+        std::unique_ptr<AdjacencyList> adjacency_list_ptr_;         ///< Vector containing all the 
+                                                                    ///< connections in the graph
+
+        std::vector<std::shared_ptr<State>> states_;                                 ///< Vector of all the states in the graph
+        
 
     public:
 
-        std::shared_ptr<fluid::State> current_state_p;            ///< The current state of the state graph.
+        std::shared_ptr<fluid::State> current_state_ptr;              ///< The current state of the state graph.
 
         /**
          * @brief      Sets up the graph with the respective states.
          */
         StateGraph();
+
+        /**
+         * Adds edges to the graph, these edges form the connections between the states in the graph.
+         *
+         * @param edges Vector of edges.
+         */
+        void addEdges(std::vector<fluid::Edge<std::shared_ptr<State>>> edges);
+
+        /**
+         * @return The states in the graph.
+         */
+        std::vector<std::shared_ptr<State>> getStates();
 
         /**
          * Checks if the start state and the end state is connected using a breadth first search and
@@ -44,6 +79,11 @@ namespace fluid {
          * @return The state with the given identifier in the graph. Will return a nullptr if not found.
          */
         std::shared_ptr<fluid::State> getStateWithIdentifier(std::string identifier);
+
+        /**
+         * Returns a stream with information about this state graph.
+         */
+        std::ostream& operator<<(std::ostream& ostream);
     };
 }
 
--- src/core/operation/state_graph.cpp
+++ src/core/operation/state_graph.cpp
@@ -16,31 +16,57 @@
 #include "move_state.h"
 #include "position_follow_state.h"
 
-fluid::StateGraph::StateGraph() : Graph() {
+fluid::StateGraph::StateGraph() {
+
+    adjacency_list_ptr_ = std::make_unique<AdjacencyList>();
 
     // Set up the graph
-    std::shared_ptr<fluid::Identifiable> init_state = std::make_shared<fluid::InitState>();
-    std::shared_ptr<fluid::Identifiable> idle_state = std::make_shared<fluid::IdleState>();
-    std::shared_ptr<fluid::Identifiable> take_off_state = std::make_shared<fluid::TakeOffState>();
-    std::shared_ptr<fluid::Identifiable> land_state = std::make_shared<fluid::LandState>();
-    std::shared_ptr<fluid::Identifiable> hold_state = std::make_shared<fluid::HoldState>();
-    std::shared_ptr<fluid::Identifiable> move_state = std::make_shared<fluid::MoveState>();
-    std::shared_ptr<fluid::Identifiable> position_follow_state = std::make_shared<fluid::PositionFollowState>();
+    std::shared_ptr<fluid::State> init_state = std::make_shared<fluid::InitState>();
+    std::shared_ptr<fluid::State> idle_state = std::make_shared<fluid::IdleState>();
+    std::shared_ptr<fluid::State> take_off_state = std::make_shared<fluid::TakeOffState>();
+    std::shared_ptr<fluid::State> land_state = std::make_shared<fluid::LandState>();
+    std::shared_ptr<fluid::State> hold_state = std::make_shared<fluid::HoldState>();
+    std::shared_ptr<fluid::State> move_state = std::make_shared<fluid::MoveState>();
+    std::shared_ptr<fluid::State> position_follow_state = std::make_shared<fluid::PositionFollowState>();
 
-    std::vector<fluid::Edge<std::shared_ptr<fluid::Identifiable>>> edges;
+    std::vector<fluid::Edge<std::shared_ptr<fluid::State>>> edges;
 
-    current_state_p = std::dynamic_pointer_cast<fluid::State>(init_state);
-    edges.emplace_back(fluid::Edge<std::shared_ptr<fluid::Identifiable>>(init_state, idle_state));
-    edges.emplace_back(fluid::Edge<std::shared_ptr<fluid::Identifiable>>(idle_state, take_off_state));
-    edges.emplace_back(fluid::Edge<std::shared_ptr<fluid::Identifiable>>(take_off_state, hold_state));
-    edges.emplace_back(fluid::Edge<std::shared_ptr<fluid::Identifiable>>(hold_state, move_state));
-    edges.emplace_back(fluid::Edge<std::shared_ptr<fluid::Identifiable>>(move_state, hold_state));
-    edges.emplace_back(fluid::Edge<std::shared_ptr<fluid::Identifiable>>(hold_state, land_state));
-    edges.emplace_back(fluid::Edge<std::shared_ptr<fluid::Identifiable>>(land_state, idle_state));
+    current_state_ptr = std::dynamic_pointer_cast<fluid::State>(init_state);
+    edges.emplace_back(fluid::Edge<std::shared_ptr<fluid::State>>(init_state, idle_state));
+    edges.emplace_back(fluid::Edge<std::shared_ptr<fluid::State>>(idle_state, take_off_state));
+    edges.emplace_back(fluid::Edge<std::shared_ptr<fluid::State>>(take_off_state, hold_state));
+    edges.emplace_back(fluid::Edge<std::shared_ptr<fluid::State>>(hold_state, move_state));
+    edges.emplace_back(fluid::Edge<std::shared_ptr<fluid::State>>(move_state, hold_state));
+    edges.emplace_back(fluid::Edge<std::shared_ptr<fluid::State>>(hold_state, land_state));
+    edges.emplace_back(fluid::Edge<std::shared_ptr<fluid::State>>(land_state, idle_state));
 
-    edges.emplace_back(fluid::Edge<std::shared_ptr<fluid::Identifiable>>(hold_state, position_follow_state));
-    edges.emplace_back(fluid::Edge<std::shared_ptr<fluid::Identifiable>>(position_follow_state, hold_state));
+    edges.emplace_back(fluid::Edge<std::shared_ptr<fluid::State>>(hold_state, position_follow_state));
+    edges.emplace_back(fluid::Edge<std::shared_ptr<fluid::State>>(position_follow_state, hold_state));
     addEdges(edges);
+}
+
+void fluid::StateGraph::addEdges(std::vector<fluid::Edge<std::shared_ptr<State>>> edges) {
+    for (auto edge : edges) {
+
+        // If the node isn't added in the adjacency list, we add the the node and its neighbour (edge destination).
+        if (adjacency_list_ptr_->find(edge.source->identifier) == adjacency_list_ptr_->end()) {
+
+            std::vector<std::shared_ptr<fluid::State>> neighbours;
+            neighbours.push_back(edge.destination);
+            adjacency_list_ptr_->insert(std::make_pair(edge.source->identifier, neighbours));
+
+            // As this node didn't exist in the graph, we add it the list of nodes
+            states_.push_back(edge.source);
+        }
+        // Node exists in graph, just add the new neighbour
+        else {
+            adjacency_list_ptr_->at(edge.source->identifier).push_back(edge.destination);
+        }
+    }
+}
+
+std::vector<std::shared_ptr<fluid::State>> fluid::StateGraph::getStates() {
+    return states_;
 }
 
 std::vector<std::shared_ptr<fluid::State>> fluid::StateGraph::getPathToEndState(std::string start_state_identifier,
@@ -51,7 +77,7 @@ std::vector<std::shared_ptr<fluid::State>> fluid::StateGraph::getPathToEndState(
     std::map<std::string, bool> visited;
     std::map<std::string, std::string> pred;
 
-    for (auto const& item : *adjacency_list) {
+    for (auto const& item : *adjacency_list_ptr_) {
         visited[item.first] = false;
         pred[item.first] = "no_val";
     }
@@ -68,7 +94,7 @@ std::vector<std::shared_ptr<fluid::State>> fluid::StateGraph::getPathToEndState(
         start_state_identifier = queue.front();
         queue.pop_front();
 
-        for(const auto &neighborState : adjacency_list->at(start_state_identifier)) {
+        for(const auto &neighborState : adjacency_list_ptr_->at(start_state_identifier)) {
             if(!visited[neighborState->identifier]) {
                 visited[neighborState->identifier] = true;
                 pred[neighborState->identifier] = start_state_identifier;
@@ -111,13 +137,26 @@ std::vector<std::shared_ptr<fluid::State>> fluid::StateGraph::getPathToEndState(
 
 std::shared_ptr<fluid::State> fluid::StateGraph::getStateWithIdentifier(std::string identifier) {
     // Get state with this identifier from the state vector
-    for (auto node : getNodes()) {
-        if (node->identifier == identifier) {
-
-            // TODO: Won't cast for some reason.
-            return std::dynamic_pointer_cast<fluid::State>(node);
+    for (auto state : getStates()) {
+        if (state->identifier == identifier) {
+            return state;
         }
     }
 
     return nullptr;
 }
+
+std::ostream& fluid::StateGraph::operator<<(std::ostream& ostream) {
+for (auto const& item : *adjacency_list_ptr_) {
+        
+        ostream << "\n Adjacency list of vertex " << item.first << "\n head ";
+        
+        for (auto &neighbor : item.second) {
+            ostream << "-> " << neighbor->identifier;
+        }
+    }
+
+    return ostream;
+}
+
+
