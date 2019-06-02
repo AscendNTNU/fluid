c164102408fb1165c91fb5a4f1fb6c8ff8a8a5e0
Simen Gangstad <simen.gangstad@me.com>
2019-06-03 00:40:51 +0200

    Retrieve params from ros instead from launch args

--- src/main.cpp
+++ src/main.cpp
@@ -13,25 +13,26 @@ int main(int argc, char** argv) {
 
     ROS_INFO("Starting Fluid FSM.");
 
-    fluid::Core::refresh_rate = static_cast<unsigned int>(atoi(argv[1]));
-    fluid::Core::message_queue_size = static_cast<unsigned int>(atoi(argv[2]));
-    fluid::Core::auto_arm = std::string(argv[3]) == "true";
-    fluid::Core::auto_set_offboard = std::string(argv[4]) == "true";
-    
 	ros::NodeHandle node_handle;
-    
-    node_handle.getParam("minX", fluid::Core::minX);
-    node_handle.getParam("minY", fluid::Core::minY);
-    node_handle.getParam("minZ", fluid::Core::minZ);
-    node_handle.getParam("maxX", fluid::Core::maxX);
-    node_handle.getParam("maxY", fluid::Core::maxY);
-    node_handle.getParam("maxZ", fluid::Core::maxZ);
 
-    node_handle.getParam("distanceCompletionThreshold", fluid::Core::distance_completion_threshold);
-    node_handle.getParam("velocityCompletionThreshold", fluid::Core::velocity_completion_threshold);
-    node_handle.getParam("yawCompletionThreshold", fluid::Core::yaw_completion_threshold);
-    node_handle.getParam("defaultHeight", fluid::Core::default_height);
-    node_handle.getParam("positionFollowHeight", fluid::Core::positionFollowHeight);
+
+    node_handle.getParam("refresh_rate", fluid::Core::refresh_rate);
+    node_handle.getParam("message_queue_size", fluid::Core::message_queue_size);
+    node_handle.getParam("auto_arm", fluid::Core::auto_arm);
+    node_handle.getParam("auto_offboard", fluid::Core::auto_set_offboard);
+
+    node_handle.getParam("min_x", fluid::Core::minX);
+    node_handle.getParam("min_y", fluid::Core::minY);
+    node_handle.getParam("min_z", fluid::Core::minZ);
+    node_handle.getParam("max_x", fluid::Core::maxX);
+    node_handle.getParam("max_y", fluid::Core::maxY);
+    node_handle.getParam("max_z", fluid::Core::maxZ);
+
+    node_handle.getParam("distance_completion_threshold", fluid::Core::distance_completion_threshold);
+    node_handle.getParam("velocity_completion_threshold", fluid::Core::velocity_completion_threshold);
+    node_handle.getParam("yaw_completion_threshold", fluid::Core::yaw_completion_threshold);
+    node_handle.getParam("default_height", fluid::Core::default_height);
+    node_handle.getParam("position_follow_height", fluid::Core::positionFollowHeight);
 
     fluid::Server server;
     server.start();
