58762d906d7ace8a6d0829def74dd768782720e8
Simen Gangstad <simen.gangstad@me.com>
2019-06-03 01:50:08 +0200

    Grab the current pose to hold at

--- src/states/hold_state.cpp
+++ src/states/hold_state.cpp
@@ -8,6 +8,13 @@ bool fluid::HoldState::hasFinishedExecution() {
     return false;
 }
 
+void fluid::HoldState::initialize() {
+    setpoint.position.x = getCurrentPose().pose.position.x;
+    setpoint.position.y = getCurrentPose().pose.position.y;
+    setpoint.position.z = getCurrentPose().pose.position.z;
+}
+
+
 void fluid::HoldState::tick() {
     setpoint.type_mask = fluid::TypeMask::Default;
 }
