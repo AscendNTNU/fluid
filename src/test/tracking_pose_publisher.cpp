9fff577bac96403043022a88ff59fd075ab64d10
Simen Gangstad <simen.gangstad@me.com>
2019-06-06 17:20:44 +0200

    Temporary set to publish position targets

--- src/test/tracking_pose_publisher.cpp
+++ src/test/tracking_pose_publisher.cpp
@@ -4,26 +4,26 @@
 
 #include <ros/ros.h>
 #include <geometry_msgs/Pose.h>
+#include <mavros_msgs/PositionTarget.h>
 
 int main(int argc, char** argv) {
 
-    ros::init(argc, argv, "fluid_fsm");
+    ros::init(argc, argv, "fluid_fsm_tracking_publisher");
     ros::NodeHandle node_handle;
     
-    ros::Publisher publisher = node_handle.advertise<geometry_msgs::Pose>("perception/target", 100);
+    ros::Publisher publisher = node_handle.advertise<mavros_msgs::PositionTarget>("obstacle_avoidance/target_position", 100);
 
-    geometry_msgs::Pose pose;
+    mavros_msgs::PositionTarget setpoint;
 
     ros::Rate rate(5);
 
     double theta = 0.0;
-    
+
+    setpoint.position.z = 1.0;
+
     while (ros::ok()) {
 
-        pose.position.x = 1.5 + 0.5*cos(theta);
-        pose.position.y = 1.5 + 0.5*sin(theta);
-
-        publisher.publish(pose);
+        publisher.publish(setpoint);
 
         theta += 0.1;
 
