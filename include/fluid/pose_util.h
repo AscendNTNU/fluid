1ad0758af0d77d9e213d3399e3ee0f75609fe806
Simen Gangstad <simen.gangstad@me.com>
2019-07-25 17:58:09 +0200

    Add distance between angles

--- include/fluid/pose_util.h
+++ include/fluid/pose_util.h
@@ -7,6 +7,10 @@
 
 #include <mavros_msgs/PositionTarget.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <tf2/transform_datatypes.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Matrix3x3.h>
+#include <geometry_msgs/Quaternion.h>
 
 namespace fluid {
     class PoseUtil {
@@ -18,6 +22,22 @@ namespace fluid {
 
             return sqrt(delta_x*delta_x + delta_y*delta_y + delta_z*delta_z);
         }
+
+        static double angleBetween(geometry_msgs::Quaternion quaternion, float yaw_angle) {
+            tf2::Quaternion quat(quaternion.x, 
+                                 quaternion.y, 
+                                 quaternion.z, 
+                                 quaternion.w);
+
+            double roll, pitch, yaw;
+            tf2::Matrix3x3(quat).getRPY(roll, pitch, yaw);
+            // If the quaternion is invalid, e.g. (0, 0, 0, 0), getRPY will return nan, so in that case we just set 
+            // it to zero. 
+            yaw = std::isnan(yaw) ? 0.0 : yaw;
+            const auto yaw_error = yaw_angle - yaw;
+
+            return std::atan2(std::sin(yaw_error), std::cos(yaw_error)); 
+        }  
     };
 }
 
