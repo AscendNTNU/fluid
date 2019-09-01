9732c194582756248613e49123d557acb664ded5
simengangstad <simen.gangstad@me.com>
2019-09-02 00:26:00 +0200

    Add callbacks to test client

--- src/test/test_client.py
+++ src/test/test_client.py
@@ -11,7 +11,19 @@ import actionlib
 import ascend_msgs.msg
 from mavros_msgs.msg import PositionTarget
 
-def fibonacci_client():
+def active_callback():
+    print("Goal active!")
+
+def feedback_callback(feedback):
+    print("Feedback - " + "Current state: " + feedback.state.data)
+    
+    # Do something with the pose: feedback.pose_stamped
+
+def done_callback(state, result):
+    print("Finshed with state: " + str(state) + "\nFinal Fluid state: " + result.state.data)
+    # Do something with the pose: feedback.pose_stamped
+
+def send_goal():
     # Creates the SimpleActionClient, passing the type of the action
     # (FibonacciAction) to the constructor.
     client = actionlib.SimpleActionClient('drone_1/fluid_operation', ascend_msgs.msg.FluidAction)
@@ -21,30 +33,26 @@ def fibonacci_client():
     client.wait_for_server()
 
     # Creates a goal to send to the action server.
-    setpoint = PositionTarget()
-    setpoint.position.x = 0;
-
-
+    # setpoint = PositionTarget()
     goal = ascend_msgs.msg.FluidGoal()
     goal.type.data = "take_off"
-    goal.setpoint = setpoint
+
+    print("Sending goal")
 
     # Sends the goal to the action server.
-    client.send_goal(goal)
+    client.send_goal(goal, active_cb=active_callback, feedback_cb=feedback_callback, done_cb=done_callback)
 
     # Waits for the server to finish performing the action.
     client.wait_for_result()
 
     # Prints out the result of executing the action
-    # return client.get_result()  # A FibonacciResult
+    return client.get_result()  
 
 if __name__ == '__main__':
     try:
         # Initializes a rospy node so that the SimpleActionClient can
         # publish and subscribe over ROS.
-        rospy.init_node('fibonacci_client_py')
-        fibonacci_client()
-        # print("Result:", ', '.join([str(n) for n in result.sequence]))
-        print("hello")
+        rospy.init_node('fluid_client')
+        print(send_goal())
     except rospy.ROSInterruptException:
         print("program interrupted before completion")
