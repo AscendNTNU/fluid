e2b1f18b54d746f6d98386db89b15f386a2bb8f5
simengangstad <simen.gangstad@me.com>
2019-09-02 00:28:35 +0200

    Remove logging

--- src/test/test_client.cpp
+++ src/test/test_client.cpp
@@ -9,16 +9,15 @@ int main(int argc, char **argv) {
 
 	ros::init(argc, argv, "test_client");
 
-	Client client("drone_1/fluid_operation", true);
-	ROS_INFO_STREAM("Waiting for server");
+	std::string drone_namespace = "drone_1";
+	Client client(drone_namespace + "/fluid_operation", true);
 	client.waitForServer();
-	ROS_INFO_STREAM("Got connection with server");
 
 	ascend_msgs::FluidGoal goal;
 	goal.type.data = "take_off";
 
 	client.sendGoal(goal);
-	client.waitForResult(ros::Duration(10.0));
+	client.waitForResult(ros::Duration(20.0));
 
 	if (client.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
 		printf("Yay! The dishes are now clean");
