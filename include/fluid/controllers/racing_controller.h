68940a7e84f8a6782e524306116bced1b91964b0
Simen Gangstad <simen.gangstad@me.com>
2019-10-17 20:17:57 +0200

    Add racing controller

---
+++ include/fluid/controllers/racing_controller.h
@@ -0,0 +1,16 @@
+#ifndef FLUID_RACING_CONTROLLER_H
+#define FLUID_RACING_CONTROLLER_H
+
+#include "controller.h"
+namespace fluid {
+    class RacingController : Controller {
+
+        public:
+
+        RacingController(const std::string& topic, const unsigned int& degree);
+
+        void tick(std::shared_ptr<const std::vector<const std::vector<float>>> spline) const override;
+    };
+}
+
+#endif
