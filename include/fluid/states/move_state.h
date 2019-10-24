2bc1ab67090c79de40ba03eaf590bb2aad83fbfd
Simen Gangstad <simen.gangstad@me.com>
2019-10-24 14:44:21 +0200

    Remove tick and unecessary functions

--- include/fluid/states/move_state.h
+++ include/fluid/states/move_state.h
@@ -16,9 +16,6 @@ namespace fluid {
 
         bool hasFinishedExecution() const override;
         void initialize() override;
-
-        ControllerType getPreferredController() const override;
-        std::vector<std::vector<double>> getSplineForPath(const std::vector<geometry_msgs::Point>& path) const override;
    };
 }
 
--- src/states/move_state.cpp
+++ src/states/move_state.cpp
@@ -9,34 +9,21 @@
 
 bool fluid::MoveState::hasFinishedExecution() const {
     // TODO: Have to check that we've been through the whole path
-    bool atPositionTarget = Util::distanceBetween(current_pose_.pose.position, path.back()) < fluid::Core::distance_completion_threshold &&
+    bool atPositionTarget = Util::distanceBetween(getCurrentPose().pose.position, path.back()) < fluid::Core::distance_completion_threshold &&
                             std::abs(getCurrentTwist().twist.linear.x) < fluid::Core::velocity_completion_threshold &&
                             std::abs(getCurrentTwist().twist.linear.y) < fluid::Core::velocity_completion_threshold &&
                             std::abs(getCurrentTwist().twist.linear.z) < fluid::Core::velocity_completion_threshold;
 
-    bool atYawTarget = std::abs(Util::angleBetween(current_pose_.pose.orientation, setpoint.yaw)) < fluid::Core::yaw_completion_threshold;
+    //bool atYawTarget = std::abs(Util::angleBetween(getCurrentPose().pose.orientation, path.yaw)) < fluid::Core::yaw_completion_threshold;
 
-    return atYawTarget && atPositionTarget;
+    return /* atYawTarget && */ atPositionTarget;
 }
 
-unsigned long factorial(unsigned int n)
-{
-    unsigned long long factorial = 1;
-
-    for (int i = 1; i <= n; ++i)
-    {
-        factorial *= i;
-    }
-
-    return factorial;
-}
-
-void fluid::MoveState::initialize()
-{
-    if (setpoint.position.z <= 0.1)
-    {
-        setpoint.position.z = fluid::Core::default_height;
+void fluid::MoveState::initialize() {
+    
+    for (auto iterator = path.begin(); iterator != path.end(); iterator++) {
+        if (iterator->z <= 0.1) {
+            iterator->z = fluid::Core::default_height;
+        }
     }
 }
-
-void fluid::MoveState::tick() {}
