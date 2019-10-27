ddec26ec3d311efc38648c22357899d09f71e546
Simen Gangstad <simen.gangstad@me.com>
2019-10-27 19:14:14 +0100

    Include controller

--- src/controller.cpp
+++ src/controller.cpp
@@ -1,6 +1,7 @@
 #include "controller.h"
 #include "util.h"
 #include "type_mask.h"
+#include "core.h"
 
 mavros_msgs::PositionTarget fluid::Controller::getSetpoint(const ControllerType preferred_controller, 
                                                            const double& time, 
@@ -21,21 +22,19 @@ mavros_msgs::PositionTarget fluid::Controller::getSetpoint(const ControllerType 
             ROS_FATAL("The default controller is set to passthrough, defaulting to position.");
 
         case ControllerType::Positional:
- 
             setpoint.position.x = Util::evaluatePolynomial(time, spline.x);
             setpoint.position.y = Util::evaluatePolynomial(time, spline.y);
             setpoint.position.z = Util::evaluatePolynomial(time, spline.z);
             setpoint.yaw = std::atan2(setpoint.position.y, setpoint.position.x);
             setpoint.type_mask = TypeMask::Position;
-            break;
+        break;
 
         case ControllerType::Velocity:
-
             setpoint.velocity.x = Util::derivePolynomial(time, spline.x);
             setpoint.velocity.y = Util::derivePolynomial(time, spline.y);
             setpoint.velocity.z = Util::derivePolynomial(time, spline.z);
             setpoint.type_mask = TypeMask::Velocity;
-            setpoint.yaw = std::atan2(setpoint.velocity.y, setpoint.velocity.x);
+            setpoint.yaw = std::atan2(setpoint.velocity.y, setpoint.velocity.x); 
             setpoint.coordinate_frame = 1;
         break;
     }
