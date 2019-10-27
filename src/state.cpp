4cde7a6034769b597b39615128a0e3bc7cea1750
Simen Gangstad <simen.gangstad@me.com>
2019-10-27 19:14:34 +0100

    Use LQR controller

--- src/state.cpp
+++ src/state.cpp
@@ -7,10 +7,16 @@
 #include <mavros_msgs/PositionTarget.h>
 #include <sensor_msgs/Imu.h>
 
+#include <ascend_msgs/SplineService.h>
+#include <ascend_msgs/LQR.h>
+
+#include <tf/tf.h>
+
 #include <utility>
 
 #include "util.h"
 #include "core.h"
+#include "LQR.h"
 
 fluid::State::State(std::string identifier,
                     std::string px4_mode,
@@ -78,6 +84,39 @@ std::vector<ascend_msgs::Spline> fluid::State::getSplinesForPath(const std::vect
     }
 }
 
+std::vector<double> calculateSpeedProfile(const std::vector<double>& yaws, const double& target_speed) {
+    std::vector<double> speed_profile(yaws.size(), target_speed);
+
+    double direction = 1.0;
+
+    for (unsigned int i = 0; i < speed_profile.size() - 1; i++) {
+        double delta_yaw = std::abs(yaws[i + 1] - yaws[i]);
+        bool toggle = M_PI / 4.0 <= delta_yaw < M_PI / 2.0;
+
+        if (toggle) {
+            direction *= -1;
+        }
+
+        if (direction != 1) {
+            speed_profile[i] = -target_speed;
+        }
+        else {
+            speed_profile[i] = target_speed;
+        }
+    }
+
+    for (unsigned int i = speed_profile.size() - 1; i > speed_profile.size() - 40; i--) {
+        speed_profile[i] = target_speed / (50 - i);
+
+        if (speed_profile[i] <= 1.0 / 3.6) {
+            speed_profile[i] = 1.0 / 3.6;
+        }
+    }
+
+    return speed_profile;
+}
+
+
 void fluid::State::perform(std::function<bool(void)> tick, bool should_halt_if_steady) {
 
     ros::Rate rate(Core::refresh_rate);
@@ -88,26 +127,84 @@ void fluid::State::perform(std::function<bool(void)> tick, bool should_halt_if_s
     std::vector<ascend_msgs::Spline> splines = getSplinesForPath(path);
     ascend_msgs::Spline current_spline = splines[0];
 
+
+    ros::ServiceClient generate_spline = node_handle.serviceClient<ascend_msgs::SplineService>("/control/spline_generator");
+
+    ascend_msgs::SplineService spline_service_message;
+    spline_service_message.request.waypoints_x = {0.0,  6.0,  12.5, 10.0, 17.5, 20.0, 25.0};
+    spline_service_message.request.waypoints_y = {0.0, -3.0, -5.0,   6.5,  3.0,  0.0,  0.0};
+    spline_service_message.request.ds = 0.1;
+    generate_spline.call(spline_service_message);
+
+    auto spline_response = spline_service_message.response; 
+    double e, e_th;
+
+    e = e_th = 0;
+
+
+    ros::ServiceClient lqr_client = node_handle.serviceClient<ascend_msgs::LQR>("/control/lqr");
+    fluid::LQR lqr;
+
     while (ros::ok() && ((should_halt_if_steady && steady) || !hasFinishedExecution()) && tick()) {
 
+         
         ros::Duration current_time = ros::Time::now() - startTime;
 
         for (auto spline : splines) {
+
             if (current_time.toSec() < spline.timestamp) {
                 current_spline = spline;
             }
         }
 
-        setpoint = Core::getControllerPtr()->getSetpoint(getPreferredController(), current_time.toSec(), current_spline);
+        if (getPreferredController() != ControllerType::Positional) {
+
+            fluid::Path path{spline_response.cx, spline_response.cy, spline_response.cyaw, spline_response.ck};
+            fluid::Result result = lqr.control_law(getCurrentPose().pose, getCurrentTwist().twist, path, e, e_th, calculateSpeedProfile(path.yaw, 10.0 / 3.6));
+
+            e = result.error;
+            e_th = result.error_in_yaw;
+
+            tf2::Quaternion quaternion(getCurrentPose().pose.orientation.x, getCurrentPose().pose.orientation.y, getCurrentPose().pose.orientation.z, getCurrentPose().pose.orientation.w);
+            double roll, pitch, yaw;
+            tf2::Matrix3x3(quaternion).getRPY(roll, pitch, yaw);
+            
+            if (!std::isfinite(yaw)) {
+                yaw = 0;
+            }
+
+            setpoint.type_mask = TypeMask::Velocity;
+            setpoint.yaw += sqrt(pow(setpoint.velocity.x, 2) + pow(setpoint.velocity.y, 2)) * tan(result.delta) * 0.05;
+            setpoint.velocity.x += result.acceleration * std::cos(yaw) * 0.05;
+            setpoint.velocity.y += result.acceleration * std::sin(yaw) * 0.05;
+            setpoint.velocity.z = 0.0;
+            setpoint.coordinate_frame = 1; 
+        }
+        else {
+            setpoint = Core::getControllerPtr()->getSetpoint(getPreferredController(), current_time.toSec(), current_spline);
+        }
 
         publishSetpoint();
 
+        geometry_msgs::Point spline_position;
+        
+        spline_position.x = Util::evaluatePolynomial(current_time.toSec(), current_spline.x);
+        spline_position.y = Util::evaluatePolynomial(current_time.toSec(), current_spline.y);
+        spline_position.z = Util::evaluatePolynomial(current_time.toSec(), current_spline.z);
+
+/*
+        if (Util::distanceBetween(spline_position, getCurrentPose().pose.position) > 3.0) {
+            // Replan
+            startTime = ros::Time::now();
+            splines = getSplinesForPath(path);
+            current_spline = splines[0];
+        }
+*/
         /*if (should_check_obstacle_avoidance_completion_ && obstacle_avoidance_completed_) {
             // Obstacle avoidance reported that we've come as far as we can in this state 
             ROS_INFO_STREAM(identifier << ": " << "OA completed");
             break;
         } */        
-
 
         fluid::Core::getStatusPublisherPtr()->status.path = path;
         fluid::Core::getStatusPublisherPtr()->publish();
