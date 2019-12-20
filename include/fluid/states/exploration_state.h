e1d44b8069d6a4654840d238146ff55dbe328362
Simen Gangstad <simen.gangstad@me.com>
2019-12-20 12:20:17 +0100

    Add a subscriber to listen to the point we will face
    This will happen during the exploration phase where AI tells control they
    want to search a given area and at the same time face towards a given
    point

--- include/fluid/states/exploration_state.h
+++ include/fluid/states/exploration_state.h
@@ -7,8 +7,18 @@
 namespace fluid {
     class ExplorationState : public MoveState {
 
+        private:
+
+            geometry_msgs::Point point_of_interest;
+            ros::Subscriber point_of_interest_subscriber;
+
+            void pointOfInterestCallback(const geometry_msgs::PointConstPtr point);
+            
         public:
             ExplorationState() : MoveState(fluid::StateIdentifier::Exploration, 1.0, 0.3, 1.0) {}
+
+            void initialize() override;
+            void tick() override;
     };
 }
 
--- src/states/exploration_state.cpp
+++ src/states/exploration_state.cpp
@@ -1,2 +1,24 @@
 #include "exploration_state.h"
 
+
+void fluid::ExplorationState::initialize() {
+    MoveState::initialize();
+
+    std::string point_of_interest_topic;
+
+    node_handle.param<std::string>("exploration_point_of_interest_topic", point_of_interest_topic, "ai/exploration_point_of_interest");
+    point_of_interest_subscriber = node_handle.subscribe<geometry_msgs::Point>(point_of_interest_topic, 1, &ExplorationState::pointOfInterestCallback);
+}
+
+void fluid::ExplorationState::pointOfInterestCallback(const geometry_msgs::PointConstPtr point) {
+   point_of_interest = *point; 
+}
+
+void fluid::ExplorationState::tick() {
+    MoveState::tick();
+
+    double dx = point_of_interest.x - getCurrentPose().pose.position.x; 
+    double dy = point_of_interest.y - getCurrentPose().pose.position.y; 
+
+    setpoint.yaw = std::atan2(dy, dx);
+}
