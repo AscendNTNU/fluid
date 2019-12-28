bdec19b49c8c515e06205bd07740412da726899a
Simen Gangstad <simen.gangstad@me.com>
2019-12-28 09:05:51 +0100

    Set up the correct frames for the paths

--- src/status_publisher.cpp
+++ src/status_publisher.cpp
@@ -18,27 +18,28 @@ StatusPublisher::StatusPublisher() {
 }
 
 void StatusPublisher::poseCallback(const geometry_msgs::PoseStampedConstPtr pose_ptr) {
-
-    if (trace_path.poses.size() > 30) {
+    if (trace_path.poses.size() > 300) {
         trace_path.poses.erase(trace_path.poses.begin());
     }
 
     trace_path.poses.push_back(*pose_ptr);
 }
 
-void StatusPublisher::publish() { 
+void StatusPublisher::publish() {
     status_publisher.publish(status);
+    trace_path.header.stamp = ros::Time::now();
+    trace_path.header.frame_id = "map";
     trace_publisher.publish(trace_path);
 
     path.poses.clear();
 
     for (auto point : status.path) {
         geometry_msgs::PoseStamped pose_stamped;
-        pose_stamped.header.frame_id = "map";
-        pose_stamped.header.stamp = ros::Time::now();
         pose_stamped.pose.position = point;
         path.poses.push_back(pose_stamped);
     }
 
+    path.header.stamp = ros::Time::now();
+    path.header.frame_id = "map";
     path_publisher.publish(path);
 }
