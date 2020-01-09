0bce7898d958a3751bee750dde7732fe9ce68079
Simen Gangstad <simen.gangstad@me.com>
2020-01-09 13:21:25 +0100

    Add service call to obstacle avoidance in explore state

--- include/fluid/states/explore_state.h
+++ include/fluid/states/explore_state.h
@@ -1,6 +1,7 @@
 #ifndef EXPLORE_STATE_H
 #define EXPLORE_STATE_H
 
+#include <ascend_msgs/ObstacleAvoidanceService.h>
 #include "move_state.h"
 #include "state_identifier.h"
 
@@ -12,11 +13,16 @@ private:
     bool retrieved_point_of_interest = false;
     geometry_msgs::Point point_of_interest;
     ros::Subscriber point_of_interest_subscriber;
+    ros::ServiceClient obstacle_avoidance_service_client;
+    std::vector<geometry_msgs::Point> original_path;
 
     void pointOfInterestCallback(const geometry_msgs::PointConstPtr& point);
 
 public:
-    ExploreState() : MoveState(StateIdentifier::Explore, 0.3, 0.3, 0.3) {}
+    ExploreState()
+        : MoveState(StateIdentifier::Explore, 0.3, 0.3, 0.3),
+          obstacle_avoidance_service_client(
+              node_handle.serviceClient<ascend_msgs::ObstacleAvoidanceService>("obstacle_avoidance/path")) {}
 
     void initialize() override;
     void tick() override;
--- src/states/explore_state.cpp
+++ src/states/explore_state.cpp
@@ -1,4 +1,6 @@
 #include "explore_state.h"
+#include "util.h"
+#include <limits>
 
 void ExploreState::initialize() {
     MoveState::initialize();
@@ -10,6 +12,8 @@ void ExploreState::initialize() {
                                    "ai/explore_point_of_interest");
     point_of_interest_subscriber = node_handle.subscribe<geometry_msgs::Point>(
         point_of_interest_topic, 1, &ExploreState::pointOfInterestCallback, this);
+
+    original_path = path;
 }
 
 void ExploreState::pointOfInterestCallback(const geometry_msgs::PointConstPtr& point) {
@@ -18,8 +22,51 @@ void ExploreState::pointOfInterestCallback(const geometry_msgs::PointConstPtr& p
 }
 
 void ExploreState::tick() {
-    MoveState::tick();
 
+    ascend_msgs::ObstacleAvoidanceService service;
+    service.request.path = original_path;
+
+    if (obstacle_avoidance_service_client.call(service)) {
+        // Check if the path is different from the current path
+
+        bool different_path = path.size() != service.response.new_path.size();
+        double closest_distance = std::numeric_limits<double>::max();
+        unsigned int closest_point_index = -1;
+
+        // Find the point we are closest to in the path given from OA and set that as starting point for the
+        // iterator if the paths are different
+        if (path.size() == service.response.new_path.size()) {
+            for (int i = 0; i < path.size(); i++) {
+                double distance = Util::distanceBetween(path[i], service.response.new_path[i]);
+
+                if (distance >= 0.01) {
+                    different_path = true;
+                }
+
+                if (distance <= closest_distance) {
+                    closest_distance = distance;
+                    closest_point_index = i;
+                }
+            }
+        }
+
+        if (different_path) {
+            if (closest_point_index != -1) {
+                path = service.response.new_path;
+                current_destination_point_iterator = path.begin() + closest_point_index;
+            }
+            else {
+                ROS_FATAL_STREAM(
+                    "Could not find a closest point even though the paths were different, did the obstacle avoidance "
+                    "service return a finite path of infinite numbers? Will not change path.");
+            }
+        }
+    } else {
+        ROS_FATAL_STREAM("Failed to call obstacle avoidance service!");
+    }
+
+    MoveState::tick();
+    
     if (retrieved_point_of_interest) {
         double dx = point_of_interest.x - getCurrentPose().pose.position.x;
         double dy = point_of_interest.y - getCurrentPose().pose.position.y;
