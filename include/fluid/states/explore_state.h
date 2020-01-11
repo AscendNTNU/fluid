cb58cb32e45c2ed2f9787d754217d064341d29fc
Simen Gangstad <simen.gangstad@me.com>
2020-01-11 18:24:43 +0100

    Add obstacle avoidance integration

--- include/fluid/states/explore_state.h
+++ include/fluid/states/explore_state.h
@@ -2,8 +2,10 @@
 #define EXPLORE_STATE_H
 
 #include <ascend_msgs/ObstacleAvoidanceService.h>
+#include <ascend_msgs/Path.h>
 #include "move_state.h"
 #include "state_identifier.h"
+#include <visualization_msgs/Marker.h>
 
 /**
  * \brief Drone is following a path and (optionally) facing towards a certain point
@@ -13,19 +15,28 @@ private:
     bool retrieved_point_of_interest = false;
     geometry_msgs::Point point_of_interest;
     ros::Subscriber point_of_interest_subscriber;
-    ros::ServiceClient obstacle_avoidance_service_client;
+    ros::Publisher obstacle_avoidance_path_publisher;
+    ros::Subscriber obstacle_avoidance_path_subscriber;
+    ros::Publisher current_setpoint_visualization_publisher;
     std::vector<geometry_msgs::Point> original_path;
+    std::vector<geometry_msgs::Point> corrected_path;
+
+    bool original_path_set = false;
 
     void pointOfInterestCallback(const geometry_msgs::PointConstPtr& point);
 
 public:
     ExploreState()
         : MoveState(StateIdentifier::Explore, 0.3, 0.3, 0.3),
-          obstacle_avoidance_service_client(
-              node_handle.serviceClient<ascend_msgs::ObstacleAvoidanceService>("obstacle_avoidance/path")) {}
+          obstacle_avoidance_path_publisher(node_handle.advertise<ascend_msgs::Path>("/obstacle_avoidance/path", 10)),
+          obstacle_avoidance_path_subscriber(node_handle.subscribe("/obstacle_avoidance/corrected_path", 10, &ExploreState::pathCallback, this)),
+          current_setpoint_visualization_publisher(node_handle.advertise<visualization_msgs::Marker>("/fluid/setpoint_visualiztion", 10)) {}
+
+    void pathCallback(ascend_msgs::Path corrected_path);
 
     void initialize() override;
     void tick() override;
+    void finalize() override;
 };
 
 #endif
--- src/states/explore_state.cpp
+++ src/states/explore_state.cpp
@@ -1,6 +1,6 @@
 #include "explore_state.h"
-#include "util.h"
 #include <limits>
+#include "util.h"
 
 void ExploreState::initialize() {
     MoveState::initialize();
@@ -14,6 +14,7 @@ void ExploreState::initialize() {
         point_of_interest_topic, 1, &ExploreState::pointOfInterestCallback, this);
 
     original_path = path;
+    original_path_set = true;
 }
 
 void ExploreState::pointOfInterestCallback(const geometry_msgs::PointConstPtr& point) {
@@ -21,56 +22,96 @@ void ExploreState::pointOfInterestCallback(const geometry_msgs::PointConstPtr& p
     retrieved_point_of_interest = true;
 }
 
-void ExploreState::tick() {
+void ExploreState::pathCallback(ascend_msgs::Path corrected_path) {
+    std::reverse(corrected_path.points.begin(), corrected_path.points.end());
 
-    ascend_msgs::ObstacleAvoidanceService service;
-    service.request.path = original_path;
-
-    if (obstacle_avoidance_service_client.call(service)) {
+    if (original_path_set) {
         // Check if the path is different from the current path
 
-        bool different_path = path.size() != service.response.new_path.size();
-        double closest_distance = std::numeric_limits<double>::max();
+        bool different_path = path.size() != corrected_path.points.size();
         unsigned int closest_point_index = -1;
 
         // Find the point we are closest to in the path given from OA and set that as starting point for the
         // iterator if the paths are different
-        if (path.size() == service.response.new_path.size()) {
+        if (path.size() == corrected_path.points.size()) {
             for (int i = 0; i < path.size(); i++) {
-                double distance = Util::distanceBetween(path[i], service.response.new_path[i]);
+                double distance = Util::distanceBetween(path[i], corrected_path.points[i]);
 
                 if (distance >= 0.01) {
                     different_path = true;
+                    break;
                 }
+            }
+        }
+            
+        if (different_path) {
+            double closest_distance = std::numeric_limits<double>::max();
+
+            for (int i = 0; i < corrected_path.points.size(); i++) {
+                double distance = Util::distanceBetween(getCurrentPose().pose.position, corrected_path.points[i]);
 
                 if (distance <= closest_distance) {
                     closest_distance = distance;
                     closest_point_index = i;
                 }
             }
-        }
 
-        if (different_path) {
             if (closest_point_index != -1) {
-                path = service.response.new_path;
+                path = corrected_path.points;
                 current_destination_point_iterator = path.begin() + closest_point_index;
-            }
-            else {
+                update_setpoint = true;
+            } else {
                 ROS_FATAL_STREAM(
                     "Could not find a closest point even though the paths were different, did the obstacle avoidance "
                     "service return a finite path of infinite numbers? Will not change path.");
             }
         }
-    } else {
-        ROS_FATAL_STREAM("Failed to call obstacle avoidance service!");
     }
+}
 
+void ExploreState::tick() {
     MoveState::tick();
-    
+
     if (retrieved_point_of_interest) {
         double dx = point_of_interest.x - getCurrentPose().pose.position.x;
         double dy = point_of_interest.y - getCurrentPose().pose.position.y;
 
         setpoint.yaw = std::atan2(dy, dx);
     }
+
+    visualization_msgs::Marker marker;
+
+    marker.header.frame_id = "/map";
+    marker.header.stamp = ros::Time::now();
+
+    marker.id = 9999;
+    marker.type = visualization_msgs::Marker::SPHERE;
+    marker.action = visualization_msgs::Marker::ADD;
+    marker.pose.position.x = current_destination_point_iterator->x;
+    marker.pose.position.y = current_destination_point_iterator->y;
+    marker.pose.position.z = current_destination_point_iterator->z;
+    marker.pose.orientation.x = 0.0;
+    marker.pose.orientation.y = 0.0;
+    marker.pose.orientation.z = 0.0;
+    marker.pose.orientation.w = 1.0;
+
+    marker.scale.x = 0.2;
+    marker.scale.y = 0.2;
+    marker.scale.z = 0.2;
+
+    // Set the color -- be sure to set alpha to something non-zero!
+    marker.color.r = 1.0f;
+    marker.color.g = 0.0f;
+    marker.color.b = 1.0f;
+    marker.color.a = 1.0;
+
+    marker.lifetime = ros::Duration();
+
+    current_setpoint_visualization_publisher.publish(marker);
+
+    ascend_msgs::Path path_msg;
+    path_msg.points = original_path;
+    obstacle_avoidance_path_publisher.publish(path_msg);
 }
+
+void ExploreState::finalize() { original_path_set = false; }
