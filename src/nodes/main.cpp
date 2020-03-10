a7be64a35d9d402d0e59b15a09991929df4a88fd
Simen Gangstad <simen.gangstad@me.com>
2020-03-10 12:36:12 +0100

    Fix checking of parameters

--- src/nodes/main.cpp
+++ src/nodes/main.cpp
@@ -19,34 +19,34 @@ int main(int argc, char** argv) {
     ROS_INFO("Starting Fluid FSM.");
 
     ros::NodeHandle node_handle;
-    const std::string prefix = ros::this_node::getName();
+    const std::string prefix = ros::this_node::getName() + "/";
 
-    if (node_handle.getParam(prefix + "refresh_rate", Core::refresh_rate)) {
+    if (!node_handle.getParam(prefix + "refresh_rate", Core::refresh_rate)) {
         exitAtParameterExtractionFailure(prefix + "refresh_rate");
     }
 
-    if (node_handle.getParam(prefix + "auto_arm", Core::auto_arm)) {
+    if (!node_handle.getParam(prefix + "auto_arm", Core::auto_arm)) {
         exitAtParameterExtractionFailure(prefix + "auto_arm");
     }
 
-    if (node_handle.getParam(prefix + "auto_offboard", Core::auto_set_offboard)) {
+    if (!node_handle.getParam(prefix + "auto_offboard", Core::auto_set_offboard)) {
         exitAtParameterExtractionFailure(prefix + "auto_offboard");
     }
 
-    if (node_handle.getParam(prefix + "distance_completion_threshold", Core::distance_completion_threshold)) {
+    if (!node_handle.getParam(prefix + "distance_completion_threshold", Core::distance_completion_threshold)) {
         exitAtParameterExtractionFailure(prefix + "distance_completion_threshold");
     }
 
-    if (node_handle.getParam(prefix + "velocity_completion_threshold", Core::velocity_completion_threshold)) {
+    if (!node_handle.getParam(prefix + "velocity_completion_threshold", Core::velocity_completion_threshold)) {
         exitAtParameterExtractionFailure(prefix + "velocity_completion_threshold");
     }
 
-    if (node_handle.getParam(prefix + "yaw_completion_threshold", Core::yaw_completion_threshold)) {
+    if (!node_handle.getParam(prefix + "yaw_completion_threshold", Core::yaw_completion_threshold)) {
         exitAtParameterExtractionFailure(prefix + "yaw_completion_threshold");
     }
 
-    if (node_handle.getParam(prefix + "default_height", Core::default_height)) {
-        exitAtParameterExtractionFailure(prefix + "default_heigh");
+    if (!node_handle.getParam(prefix + "default_height", Core::default_height)) {
+        exitAtParameterExtractionFailure(prefix + "default_height");
     }
 
     dynamic_reconfigure::Server<fluid::ServerConfig> dynamic_reconfigure_server;
