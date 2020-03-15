8ac7ae88a50928ca765dcdab10a1cb490e4c0481
Simen Gangstad <simen.gangstad@me.com>
2020-03-15 22:46:27 +0100

    Remove state graph

--- src/core.cpp
+++ src/core.cpp
@@ -4,21 +4,12 @@ int Core::refresh_rate = 20;
 bool Core::auto_arm = false;
 bool Core::auto_set_offboard = false;
 
-std::shared_ptr<StateGraph> Core::graph_ptr;
 std::shared_ptr<StatusPublisher> Core::status_publisher_ptr;
 
 double Core::distance_completion_threshold = 0.3;
 double Core::velocity_completion_threshold = 0.1;
 double Core::yaw_completion_threshold = 0.2;
 double Core::default_height = 1.0;
-
-std::shared_ptr<StateGraph> Core::getGraphPtr() {
-    if (!graph_ptr) {
-        graph_ptr = std::make_shared<StateGraph>();
-    }
-
-    return graph_ptr;
-}
 
 std::shared_ptr<StatusPublisher> Core::getStatusPublisherPtr() {
     if (!status_publisher_ptr) {
