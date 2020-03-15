6bce014b4534d94ade84a621fc5c7c707072bbfc
Simen Gangstad <simen.gangstad@me.com>
2020-03-15 22:48:39 +0100

    Add test for multiple operations

--- src/test/test_client.py
+++ src/test/test_client.py
@@ -7,9 +7,12 @@ import ascend_msgs.msg
 from fluid.srv import *
 from geometry_msgs.msg import Point
 
+finished_state = ""
+
 
 def fluidOperationCompletionCallback(request):
     print "Finished operation " + request.operation
+    finished_state = request.operation
 
     return OperationCompletionResponse()
 
@@ -22,12 +25,32 @@ if __name__ == '__main__':
             "fluid/operation_completion", OperationCompletion, fluidOperationCompletionCallback)
 
         rospy.wait_for_service('fluid/take_off')
+        rospy.wait_for_service('fluid/explore')
 
         take_off = rospy.ServiceProxy('fluid/take_off', TakeOff)
+        explore = rospy.ServiceProxy('fluid/explore', Explore)
+
         response = take_off(3)
 
         if (not response.success):
             print response.message
+
+        while not rospy.is_shutdown():
+
+            if finished_state == "TAKE_OFF":
+                rospy.logerr(finished_state)
+                point = Point()
+                point.x = 10
+
+                # Doesnt get fired somehow
+
+                rospy.logerr("hello")
+                response = explore([point])
+
+                if (not response.success):
+                    print response.message
+
+                break
 
         rospy.spin()
 
