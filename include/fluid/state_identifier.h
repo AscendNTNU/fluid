23dd65c56af98568848b1126fd055613d47cd468
Simen Gangstad <simen.gangstad@me.com>
2020-03-22 17:47:04 +0100

    Fix mode for land

--- include/fluid/state_identifier.h
+++ include/fluid/state_identifier.h
@@ -9,7 +9,7 @@
 #include <string>
 
 const std::string PX4_MODE_OFFBOARD = "OFFBOARD";
-const std::string PX4_MODE_LAND = "MODE";
+const std::string PX4_MODE_LAND = "AUTO.LAND";
 
 /**
  * @brief Represents the different states.
@@ -27,49 +27,29 @@ enum class StateIdentifier {
 };
 
 /**
- * @brief Retrieves the PX4 mode for a given @p state_identifier. 
- * 
- * @param state_identifier The state identifier to get the PX4 mode for. 
- * 
+ * @brief Retrieves the PX4 mode for a given @p state_identifier.
+ *
+ * @param state_identifier The state identifier to get the PX4 mode for.
+ *
  * @return The PX4 mode.
  */
 std::string getPX4ModeForStateIdentifier(const StateIdentifier& state_identifier);
 
 /**
- * @brief Get a string from the @p state_identifier. 
- * 
- * @param state_identifier The state identifier. 
- * 
+ * @brief Get a string from the @p state_identifier.
+ *
+ * @param state_identifier The state identifier.
+ *
  * @return The state identifier represented in a string.
  */
 std::string getStringFromStateIdentifier(const StateIdentifier& state_identifier);
 
 /**
- * @brief Constructs a state pointer from the @p state_identifier. 
- * 
+ * @brief Constructs a state pointer from the @p state_identifier.
+ *
  * @param state_identifier The state identifier to construct the state from.
- * 
- * @return The state pointer. 
+ *
+ * @return The state pointer.
  */
-/*std::shared_ptr<State> constructStateFromStateIdentifier(const StateIdentifier& state_identifier) {
-    switch (state_identifier) {
-        case StateIdentifier::TAKE_OFF:
-            return std::make_shared<TakeOffState>();
-        case StateIdentifier::HOLD:
-            return std::make_shared<HoldState>();
-        case StateIdentifier::EXPLORE:
-            return std::make_shared<ExploreState>();
-        case StateIdentifier::TRAVEL:
-            return std::make_shared<TravelState>();
-        case StateIdentifier::LAND:
-            return std::make_shared<LandState>();
-        case StateIdentifier::EXTRACT_MODULE:
-            return std::make_shared<ExtractModuleState>();
-        case StateIdentifier::FOLLOW_MAST:
-            return std::make_shared<FollowMastState>();
-        case StateIdentifier::UNDEFINED:
-            return nullptr;
-    }
-}*/
 
 #endif
