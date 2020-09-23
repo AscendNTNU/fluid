40ac3df893166b45d6643d4970d99a6702c4d731
Simen Gangstad <simen.gangstad@me.com>
2020-09-23 08:31:44 +0200

    Add extraction logic

--- include/fluid/operations/extract_module_operation.h
+++ include/fluid/operations/extract_module_operation.h
@@ -5,6 +5,8 @@
 #ifndef EXTRACT_MODULE_OPERATION_H
 #define EXTRACT_MODULE_OPERATION_H
 
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+
 #include "operation.h"
 #include "operation_identifier.h"
 
@@ -12,12 +14,40 @@
  * @brief Represents the operation where the drone is extracting the module.
  */
 class ExtractModuleOperation : public Operation {
+   private:
+	
+	enum class ModuleState {
+		APPROACHING,
+		OVER,
+		BEHIND_WITH_HOOKS,
+	 	EXTRACTING,
+		EXTRACTED
+	};
+
+	ModuleState module_state = ModuleState::APPROACHING;
+
+    const float speed = 0.3;
+
+    geometry_msgs::PoseWithCovarianceStamped module_pose;
+
+    ros::Subscriber module_pose_subscriber;
+
+    void modulePoseCallback(const geometry_msgs::PoseWithCovarianceStampedConstPtr module_pose);
+
+    ros::ServiceClient backpropeller_client;
+
+    bool called_backpropeller_service = false;
+
    public:
     /**
-     * @brief Sets up the extract moduel operation.
-     *
+     * @brief Sets up the subscriber for the module pose.
      */
     explicit ExtractModuleOperation();
+
+    /**
+     * @brief Sets up #speed at which to move.
+     */
+    void initialize() override;
 
     /**
      * @return true When the module has been extracted.
@@ -25,9 +55,9 @@ class ExtractModuleOperation : public Operation {
     bool hasFinishedExecution() const override;
 
     /**
-     * @brief Initializes the extract module operation.
+     * @brief Makes sure the drone is following the module and reacting to the extraction signal.
      */
-    void initialize() override;
+    void tick() override;
 };
 
 #endif
