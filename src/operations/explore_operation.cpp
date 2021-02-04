8e3061e7cfc57a27cb99b67c4f55f0997b27a797
Nordtveit <sindex82@gmail.com>
2021-02-04 12:14:25 +0100

    Added point_of_interest

--- src/operations/explore_operation.cpp
+++ src/operations/explore_operation.cpp
@@ -8,33 +8,39 @@
 
 #include "util.h"
 
-ExploreOperation::ExploreOperation(const std::vector<geometry_msgs::Point>& path)
+ExploreOperation::ExploreOperation(const std::vector<geometry_msgs::Point> &path)
     : MoveOperation(OperationIdentifier::EXPLORE, path, 500, 0.5, 1),
       obstacle_avoidance_path_publisher(node_handle.advertise<ascend_msgs::Path>("/obstacle_avoidance/path", 10)),
       obstacle_avoidance_path_subscriber(
           node_handle.subscribe("/obstacle_avoidance/corrected_path", 10, &ExploreOperation::pathCallback, this)) {}
 
-void ExploreOperation::initialize() {
+void ExploreOperation::initialize()
+{
     MoveOperation::initialize();
 
     original_path = path;
     original_path_set = true;
+    point_of_interest = original_path.back();
 
     dense_path.clear();
 
-    if (original_path.size() == 1) {
+    if (original_path.size() == 1)
+    {
         dense_path.insert(dense_path.begin(), getCurrentPose().pose.position);
     }
 
-    for (int i = 1; i < original_path.size(); i++) {
+    for (int i = 1; i < original_path.size(); i++)
+    {
         std::vector<geometry_msgs::Point> filler_points =
             Util::createPath(original_path[i - 1], original_path[i], path_density);
         dense_path.insert(dense_path.end(), begin(filler_points), end(filler_points));
     }
 }
 
-void ExploreOperation::pathCallback(ascend_msgs::Path corrected_path) {
-    if (original_path_set) {
+void ExploreOperation::pathCallback(ascend_msgs::Path corrected_path)
+{
+    if (original_path_set)
+    {
         // Check if the path is different from the current path
 
         bool different_path = path.size() != corrected_path.points.size();
@@ -42,36 +48,44 @@ void ExploreOperation::pathCallback(ascend_msgs::Path corrected_path) {
 
         // Find the point we are closest to in the path given from OA and set that as starting point for the
         // iterator if the paths are different
-        if (path.size() == corrected_path.points.size()) {
-            for (int i = 0; i < path.size(); i++) {
+        if (path.size() == corrected_path.points.size())
+        {
+            for (int i = 0; i < path.size(); i++)
+            {
                 double distance = Util::distanceBetween(path[i], corrected_path.points[i]);
 
-                if (distance >= 0.01) {
+                if (distance >= 0.01)
+                {
                     different_path = true;
                     break;
                 }
             }
         }
 
-        if (different_path) {
+        if (different_path)
+        {
             double closest_distance = std::numeric_limits<double>::max();
 
-            for (int i = 0; i < corrected_path.points.size(); i++) {
+            for (int i = 0; i < corrected_path.points.size(); i++)
+            {
                 double distance = Util::distanceBetween(getCurrentPose().pose.position, corrected_path.points[i]);
 
-                if (distance <= closest_distance) {
+                if (distance <= closest_distance)
+                {
                     closest_distance = distance;
                     closest_point_index = i;
                 }
             }
 
-            if (closest_point_index != -1) {
+            if (closest_point_index != -1)
+            {
                 path = std::vector<geometry_msgs::Point>(corrected_path.points.begin() + closest_point_index,
                                                          corrected_path.points.end());
                 current_setpoint_iterator = path.begin();
                 update_setpoint = true;
-
-            } else {
+            }
+            else
+            {
                 ROS_FATAL_STREAM(ros::this_node::getName().c_str()
                                  << ": Could not find a closest point even though the paths were different, "
                                  << "did the obstacle avoidance service return a finite path of infinite "
@@ -81,13 +95,17 @@ void ExploreOperation::pathCallback(ascend_msgs::Path corrected_path) {
     }
 }
 
-void ExploreOperation::tick() {
+void ExploreOperation::tick()
+{
     MoveOperation::tick();
 
-    double dx = dense_path.back().x - getCurrentPose().pose.position.x;
-    double dy = dense_path.back().y - getCurrentPose().pose.position.y;
+    //Testing new point... -Sindre
+    double dx = point_of_interest.x - getCurrentPose().pose.position.x;
+    double dy = point_of_interest.y - getCurrentPose().pose.position.y;
+
+    //double dx = dense_path.back().x - getCurrentPose().pose.position.x;
+    //double dy = dense_path.back().y - getCurrentPose().pose.position.y;
     setpoint.yaw = std::atan2(dy, dx);
-   
 
     ascend_msgs::Path path_msg;
     path_msg.points = dense_path;
