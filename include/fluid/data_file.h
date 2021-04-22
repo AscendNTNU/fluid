acf6f0d648d127bf0737f6fd167fba34c13e764d
theodu33160 <theo.degeorges@gmail.com>
2021-04-22 13:38:59 +0200

    add doxygen comment

--- include/fluid/data_file.h
+++ include/fluid/data_file.h
@@ -30,7 +30,12 @@ class DataFile {
     public:
     DataFile(std::string name="noname.txt",std::string path = "");
 
-    void init(std::string title);  //void initSetpointLog();
+    /**
+     * @brief initialize the data file with a title. 
+     * 
+     * @param title array of names separated by tabulations
+     */
+    void init(std::string title);
     
     void initStateLog(); // initLog
 
