b9f90aca522e933c5dc4fbf13d8aec9824164fbe
JakobJern <jakobj@stud.ntnu.no>
2022-02-11 15:06:26 +0000

    Not working, more important things to fix,  might need a rewrite

--- src/nodes/base_link_publisher.cpp
+++ src/nodes/base_link_publisher.cpp
@@ -1,4 +1,6 @@
 #include <memory>
+#include <nav_msgs/msg/detail/odometry__struct.hpp>
+#include <rclcpp/node.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include "geometry_msgs/msg/pose_stamped.hpp"
 #include <tf2_ros/transform_broadcaster.h>
@@ -7,11 +9,11 @@
 
 static geometry_msgs::msg::TransformStamped transform_stamped;
 
-void poseCallback(const nav_msgs::msg::Odometry::SharedPtr &msg) {
-    static tf2_ros::TransformBroadcaster broadcaster;
+void poseCallback(const nav_msgs::msg::Odometry::SharedPtr &msg, rclcpp::Node node) {
+    static tf2_ros::TransformBroadcaster broadcaster= tf2_ros::TransformBroadcaster(node);
 
     std::chrono::time_point<std::chrono::system_clock> now = std::chrono::system_clock::now();
-    transform_stamped.header.stamp = now.duration;
+    transform_stamped.header.stamp = now;
     transform_stamped.header.frame_id = msg->header.frame_id;
     transform_stamped.transform.translation.x = msg->pose.pose.position.x;
     transform_stamped.transform.translation.y = msg->pose.pose.position.y;
@@ -26,12 +28,12 @@ void poseCallback(const nav_msgs::msg::Odometry::SharedPtr &msg) {
 }
 
 int main(int argc, char** argv) {
-    rclcpp::init(argc, argv, "base_link_tf_publisher");
+    rclcpp::init(argc, argv);
 
     // transform_stamped.header.frame_id = "odom";
     transform_stamped.child_frame_id = "base_link";
-    auto node = std::make_shared<typename _Tp>(_Args &&__args...)
-    rclcpp::Subscription<nav_msgs::Odometry::ConstPtr>::SharedPtr pose_subscriber = subscribe("/mavros/global_position/local", 10, &poseCallback);
+    std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("base_link_publisher");
+    auto pose_subscriber = node->create_subscription<nav_msgs::msg::Odometry>("/mavros/global_position/local", 10, &poseCallback);
 
     rclcpp::spin(node);
 
